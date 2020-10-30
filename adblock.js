d458e9f4d8d55832efbe45c5b89fdfd6f9590b43
redangry <54486514+redangry@users.noreply.github.com>
2020-10-29 23:13:08 -0400

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -1,17 +1,3 @@
-// twitch-videoad.js application/javascript
-// (function() {
-// 	if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
-// 	var realFetch = window.fetch;
-// 	window.fetch = function(input, init) {
-// 		if ( arguments.length >= 2 && typeof input === 'string' && input.includes('/access_token') ) {
-// 			var url = new URL(arguments[0]);
-// 			url.searchParams.set("player_type", "frontpage");
-// 			arguments[0] = url.href;
-// 		}
-// 		return realFetch.apply(this, arguments);
-// 	};
-// })();
-
 /// twitch-videoad.js
 const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
